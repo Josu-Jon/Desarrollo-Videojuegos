6f2e7fdbfba5acfb92c096eaf094d2900be0a542
Josu Jon Lopez Arribas <josujon.l.a@opendeusto.es>
2023-03-09 09:14:34 +0100

    Modificado Salto
    Salto completo y rotacion a medias

--- Assets/Scripts/saltar.cs
+++ Assets/Scripts/saltar.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 public class saltar : MonoBehaviour
 {
     [Serialize]
-    int FuerzaSalto = 1;
+    public int FuerzaSalto = 2;
+    [Serialize]
+    public int FuerzaMovimiento = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,13 +20,50 @@ public class saltar : MonoBehaviour
     void Update()
     {
         Movement();
+        Rotation();
     }
 
     private void Movement()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            this.GetComponent<Rigidbody>().AddForce(transform.up * FuerzaSalto);
+            this.GetComponent<Rigidbody>().AddForce(Vector3.up * FuerzaSalto);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.GetComponent<Rigidbody>().AddForce(Vector3.up * FuerzaMovimiento);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.GetComponent<Rigidbody>().AddForce(Vector3.down * FuerzaMovimiento);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.GetComponent<Rigidbody>().AddForce(Vector3.left * FuerzaMovimiento);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.GetComponent<Rigidbody>().AddForce(Vector3.right * FuerzaMovimiento);
+        }
+    }
+
+    private void Rotation()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.GetComponent<Transform>().transform.rotation *= Quaternion.Euler(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.GetComponent<Transform>().transform.rotation *= Quaternion.Euler(0, -1, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.GetComponent<Transform>().transform.rotation *= Quaternion.Euler(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.GetComponent<Transform>().transform.rotation *= Quaternion.Euler(0, 0, -1);
         }
     }
 }
